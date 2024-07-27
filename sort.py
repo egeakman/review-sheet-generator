79014edb4f021e26f865e1d9ff7f9307590775cd
egeakman <me@egeakman.dev>
2024-07-27 23:55:18 +0300

    don't touch keys, only sort values

--- sort.py
+++ sort.py
@@ -7,7 +7,9 @@ class Sort:
         if isinstance(data, dict):
             return {
                 key: Sort.sort_nested(value, sort_keys)
-                for key, value in sorted(data.items())
+                if isinstance(value, (dict, list))
+                else value
+                for key, value in data.items()
             }
         elif isinstance(data, list):
             if all(isinstance(item, dict) for item in data):
@@ -16,6 +18,11 @@ class Sort:
                     key=get_sort_key,
                 )
             else:
-                return sorted(Sort.sort_nested(item, sort_keys) for item in data)
+                return sorted(
+                    Sort.sort_nested(item, sort_keys)
+                    if isinstance(item, (dict, list))
+                    else item
+                    for item in data
+                )
         else:
             return data
