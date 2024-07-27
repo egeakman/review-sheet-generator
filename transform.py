b0a959cad8f06ec11dd5f72cc443e97cf59946df
egeakman <me@egeakman.dev>
2024-07-27 03:14:28 +0300

    https

--- transform.py
+++ transform.py
@@ -16,7 +16,7 @@ def session_to_youtube(session: Session) -> YouTubeVideo:
 {session.abstract}
 
 This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
-http://creativecommons.org/licenses/by-nc-sa/4.0/
+https://creativecommons.org/licenses/by-nc-sa/4.0/
 """
 
     return YouTubeVideo(title=title, description=description)
