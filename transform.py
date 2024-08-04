140108bffab63785693c2303a41914202e184dac
hypha <ramble.dalloway@gmail.com>
2024-08-04 17:27:11 +0100

    reformat licence again

--- transform.py
+++ transform.py
@@ -15,6 +15,8 @@ def session_to_video_review_sheet_item(session: Session) -> VideoReviewSheetItem
 
 {session.abstract}
 
+
+---
 This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License: https://creativecommons.org/licenses/by-nc-sa/4.0/"""
 
     video_review_sheet_item = VideoReviewSheetItem(
